ead75864d39b1f16719667f74327e8e6baf1ce57
James Post <avilapost93@gmail.com>
2021-03-04 14:14:25 -0800

    red shift- ping test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css'; 
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
+        <p className="text-red-400">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
