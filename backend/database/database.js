4bbc4cb02a2ac72b66de742f807db2c5e7c9690e
Xavier Bergeron <xavbergeron@gmail.com>
2021-03-04 21:40:45 -0800

    Database stuff

---
+++ backend/database/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/topshot", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log)
+  .catch(console.error)
+
+export default mongoose.connection
