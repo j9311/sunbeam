040e4656277ba8fed3cf6d1bcc4769ad582246cd
Xavier Bergeron <xavbergeron@gmail.com>
2021-03-11 08:08:32 -0800

    Fix dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,6 +7,8 @@ import { useQuery, gql } from "@apollo/client"
 import { CircleLoader } from "react-spinners"
 import { useBarcode } from "react-barcodes"
 
+import CodexPreview from "./CodexPreview"
+
 const GET_SETS = gql`
   query GetSets {
     getAllSets {
@@ -18,44 +20,6 @@ const GET_SETS = gql`
     }
   }
 `
-
-const RARITIES = {
-  SET_VISUAL_COMMON: "rarity-common",
-  SET_VISUAL_RARE: "rarity-rare",
-  SET_VISUAL_LEGENDARY: "rarity-legendary",
-  SET_VISUAL_ULTIMATE: "rarity-ultimate",
-}
-
-function Moment({ id, name, image, rarity, uniqueMoments }) {
-  const { inputRef } = useBarcode({
-    value: name.substr(0, 10),
-    options: {
-      lineColor: "#f0f0f0",
-      background: "#00000000",
-      displayValue: false,
-      height: 32,
-    },
-  })
-  return (
-    <div className="w-full md:w-1/3 lg:w-1/4 p-12 text-gray-50 select-none">
-      <div class={`group relative cursor-pointer card ${RARITIES[rarity]}`}>
-        <div className="absolute top-0 bottom-0 w-full rarity-round"></div>
-        <img src={image} alt={name} className="rounded-xl rarity z-0" />
-        <div className="rounded-xl absolute top-0 bottom-0 w-full z-10 flex flex-col justify-center bg-black bg-opacity-40 group-hover:bg-opacity-10">
-          <div className="bg-black bg-opacity-80 text-center py-2">
-            <h3 className="font-bungee text-xl">{name}</h3>
-            <p>{uniqueMoments} Moments</p>
-            <svg
-              ref={inputRef}
-              className="w-full"
-              preserveAspectRatio="false"
-            ></svg>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 function Dashboard(props) {
   const { loading, error, data } = useQuery(GET_SETS, {
@@ -90,7 +54,7 @@ function Dashboard(props) {
         ) : (
           <div className="container mx-auto flex flex-wrap justify-evenly">
             {data.getAllSets.map((set) => (
-              <Moment key={set.id} {...set} />
+              <CodexPreview key={set.id} {...set} />
             ))}
           </div>
         )}
