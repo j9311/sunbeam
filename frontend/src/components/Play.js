a583d7f0751967adbc67111046bbdac16656674b
Xavier Bergeron <xavbergeron@gmail.com>
2021-03-12 09:02:42 -0800

    Update fav search

--- frontend/src/components/Play.js
+++ frontend/src/components/Play.js
@@ -21,56 +21,56 @@ const Play = ({
     AddFavorite(setID, playID)
     setIsFavorite(IsFavorite(setID, playID))
   }
-
+  if (fav && !isFavorite) {
+    return null
+  }
   return (
-    (!fav || isFavorite) && (
-      <tr>
-        <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
-          <div class="flex items-center">
-            <div class="flex-shrink-0 h-10 w-10">
-              <img class="h-15 w-15 rounded-full" src={image} alt="set_im" />
-            </div>
-            <div class="ml-4">
-              <div class="text-sm text-gray-100 font-semibold">{name}</div>
-              <div class="text-sm text-gray-50 font-thin">{team}</div>
-            </div>
+    <tr>
+      <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
+        <div class="flex items-center">
+          <div class="flex-shrink-0 h-10 w-10">
+            <img class="h-15 w-15 rounded-full" src={image} alt="set_im" />
           </div>
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
-          <div class="text-sm text-gray-100 font-semibold">{set.name}</div>
-          <div class="text-sm text-gray-50 font-thin">
-            {new Date(date).toDateString()}
+          <div class="ml-4">
+            <div class="text-sm text-gray-100 font-semibold">{name}</div>
+            <div class="text-sm text-gray-50 font-thin">{team}</div>
           </div>
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
-          <span class="px-2 uppercase inline-flex font-normal justify-center text-xs leading-5 rounded-full bg-yellow-700 text-gray-50">
-            {set.humanRarity}
-          </span>
-        </td>
-        <td class="px-6 py-4 whitespace-nowrap bg-gray-800 text-sm text-gray-50">
-          <div>
-            <p>Type: {playType}</p>
-            {playType !== playCategory && <p>Category: {playCategory}</p>}
-          </div>
-        </td>
-        <td class="m-3 whitespace-nowrap text-center text-sm font-medium bg-gray-800">
-          <Link
-            to={`/moment-spec/${setID}/${playID}`}
-            type="submit"
-            class="inline bg-gray-900py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            View Moment
-          </Link>
+        </div>
+      </td>
+      <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
+        <div class="text-sm text-gray-100 font-semibold">{set.name}</div>
+        <div class="text-sm text-gray-50 font-thin">
+          {new Date(date).toDateString()}
+        </div>
+      </td>
+      <td class="px-6 py-4 whitespace-nowrap bg-gray-800">
+        <span class="px-2 uppercase inline-flex font-normal justify-center text-xs leading-5 rounded-full bg-yellow-700 text-gray-50">
+          {set.humanRarity}
+        </span>
+      </td>
+      <td class="px-6 py-4 whitespace-nowrap bg-gray-800 text-sm text-gray-50">
+        <div>
+          <p>Type: {playType}</p>
+          {playType !== playCategory && <p>Category: {playCategory}</p>}
+        </div>
+      </td>
+      <td class="m-3 whitespace-nowrap text-center text-sm font-medium bg-gray-800">
+        <Link
+          to={`/moment-spec/${setID}/${playID}`}
+          type="submit"
+          class="inline bg-gray-900py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          View Moment
+        </Link>
 
-          <button
-            onClick={favorite}
-            className="inline group relative w-5em ml-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-500"
-          >
-            Remove
-          </button>
-        </td>
-      </tr>
-    )
+        <button
+          onClick={favorite}
+          className="inline group relative w-5em ml-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-500"
+        >
+          Remove
+        </button>
+      </td>
+    </tr>
   )
 }
 
